e184ceda5924f58cb837dcd931d63aacda68324c
Matthew Dawson <mattdawson@ucar.edu>
2021-08-09 15:52:56 -0700

    add layer thickness to env state

--- src/environmental_state.F90
+++ src/environmental_state.F90
@@ -7,6 +7,8 @@
 !> The environmental_state_t type and related functions
 module ai_environmental_state
 
+  use ai_constants,                    only : kDouble
+
   implicit none
   private
 
@@ -15,6 +17,37 @@ module ai_environmental_state
   !> A collection of indenendent environmental states
   type :: environmental_state_t
     private
+    real(kind=kDouble) :: layer_thickness__Pa_ = 0.0_kDouble
+  contains
+    procedure :: set_layer_thickness__Pa
+    procedure :: layer_thickness__Pa
   end type environmental_state_t
+
+contains
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  !> Sets the layer thickness [Pa]
+  subroutine set_layer_thickness__Pa( this, layer_thickness__Pa )
+
+    class(environmental_state_t), intent(inout) :: this
+    real(kind=kDouble),           intent(in)    :: layer_thickness__Pa
+
+    this%layer_thickness__Pa_ = layer_thickness__Pa
+
+  end subroutine set_layer_thickness__Pa
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  !> Returns the layer thickness [Pa]
+  real(kind=kDouble) elemental function layer_thickness__Pa( this )
+
+    class(environmental_state_t), intent(in) :: this
+
+    layer_thickness__Pa = this%layer_thickness__Pa_
+
+  end function layer_thickness__Pa
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 end module ai_environmental_state
